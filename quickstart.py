1946c0293e46976dcc22be3821f00d3fac052aa6
Daniel Cruz <adm.dcf@gmail.com>
2021-06-19 22:20:25 -0300

    Quickstart

---
+++ quickstart.py
@@ -0,0 +1,14 @@
+#  The “Hello World” of Asyncio
+
+import asyncio, time
+
+
+async def main():
+    print(f"{time.ctime()} Hello!")
+    await asyncio.sleep(1.0)
+    print(f"{time.ctime()} Goodbye!")
+
+
+asyncio.run(
+    main()
+)  # asyncio provides a run() function to execute an async def function
